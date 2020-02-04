be71af4521e774efd8df09c296a49c9a9a52f461
zwyak <vlad.senyuk97@gmail.com>
2020-02-04 10:21:15 +0300

    added copy file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const fs = require('fs');
 const path = process.argv[2];
+
+fs.copyFile('summary.js', path + "summary.js", (err) =>{
+  if (err) throw err;
+  console.log("File was copied to destination")
+});
