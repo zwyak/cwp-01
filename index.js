7a7305a1825597464297f9e75f786dcaf6eda2e5
zwyak <vlad.senyuk97@gmail.com>
2020-02-04 16:34:02 +0300

    Add copytxt function

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const fs = require('fs');
+const path = require('path');
+
 const dir = process.argv[2];
+const folder = 'txt';
+const newdir = dir + folder;
 
 fs.copyFile('summary.js', dir + "summary.js", (err) =>{
   if (err) throw err;
   console.log("File was copied to destination")
 });
+
+fs.exists(dir + folder, (exists) => {
+  if (exists) {
+    console.error('Directory already exists');
+    copytxt();
+  } else {
+    fs.mkdir(dir + folder, { recursive: true }, (err) => {
+      if (err) throw err;
+      copytxt();
+      console.log("Directory was made to destination")
+    });
+  }
+});
+
+
+function copytxt(){
+  fs.readdir(__dirname, function(err, list) {
+      if (err) return done(err);
+
+      for (var i = 0; i < list.length; i++) {
+        if (path.extname(list[i]) == '.txt'){
+          fs.copyFile(path.basename(list[i]), newdir + '/' + path.basename(list[i]), (err) =>{
+            if (err) throw err;
+          });
+        }
+      }
+
+      console.log("Files (txt) were copied to destination")
+  });
+}
