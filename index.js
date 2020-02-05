5622ef437db7f1f657e3c813cbe48b2b28677e9c
zwyak <vlad.senyuk97@gmail.com>
2020-02-05 12:46:25 +0300

    Refactored Index.js

--- index.js
+++ index.js
@@ -6,41 +6,62 @@ const folder = 'txt';
 const newdir = dir + folder;
 const copyright = require('./config.json');
 
-fs.copyFile('summary.js', dir + "summary.js", (err) =>{
-  if (err) throw err;
-  console.log("File was copied to destination")
-});
+function copyFile(fileFrom, fileTo, callback){
+  fs.copyFile(fileFrom, fileTo, callback);
+}
 
-fs.exists(dir + folder, (exists) => {
-  if (exists) {
-    console.error('Directory already exists');
-    copytxt();
-  } else {
-    fs.mkdir(dir + folder, { recursive: true }, (err) => {
-      if (err) throw err;
-      copytxt();
-      console.log("Directory was made to destination")
+function callback() {
+  return (err) => {
+    if (err) throw err;
+    console.log("File was copied to destination")
+  }
+}
+
+fs.exists(dir, (exists) => {
+  if (exists){
+    copyFile('summary.js', path.join(dir, "summary.js"), callback());
+
+    fs.exists(newdir, (exists) => {
+      if (exists) {
+        console.error('Directory already exists');
+        copytxt();
+      } else {
+        fs.mkdir(dir + folder, { recursive: true }, (err) => {
+          if (err) throw err;
+          copytxt();
+          console.log("Directory was made to destination")
+        });
+      }
     });
+
+    function copytxt(){
+      fs.readdir(__dirname, function(err, list) {
+          if (err) return done(err);
+
+          for (var i = 0; i < list.length; i++) {
+            if (path.extname(list[i]) == '.txt'){
+              const str = path.join(newdir,  path.basename(list[i]));
+              fs.copyFile(path.basename(list[i]), str, (err) =>{
+                if (err) throw err;
+
+                fs.appendFile(str, '\r\n' + copyright.copyright, (err) => {
+                  if (err) throw err;
+                  console.log('File was written');
+                });
+              });
+            }
+          }
+
+          fs.watch(newdir, (event, filename) => {
+            if (filename) {
+              console.log(`${filename} file Changed`);
+            }
+          });
+
+          console.log("Files (txt) were copied to destination")
+      });
+    }
+  } else {
+    console.log("Input params aren't correctly");
   }
 });
-
-function copytxt(){
-  fs.readdir(__dirname, function(err, list) {
-      if (err) return done(err);
-
-      for (var i = 0; i < list.length; i++) {
-        if (path.extname(list[i]) == '.txt'){
-          fs.copyFile(path.basename(list[i]), newdir + '/' + path.basename(list[i]), (err) =>{
-            if (err) throw err;
-
-            fs.appendFile(newdir + '/' + path.basename(list[i]), copyright.copyright, (err) => {
-              if (err) throw err;
-            });
-
-          });
-        }
-      }
-
-      console.log("Files (txt) were copied to destination")
-  });
-}
